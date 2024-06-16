0bf3b2ed80d1710f85c22ab5d58fc2782226c5d1
Mahipatel7 <145039790+Mahipatel7@users.noreply.github.com>
2024-06-16 15:42:37 -0400

    Add files via upload

---
+++ advjavaassignment1/src/main/java/com/example/advjavaassignment1/DBUtility.java
@@ -0,0 +1,62 @@
+package com.example.advjavaassignment1;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.*;
+
+public class DBUtility {
+
+    private static final String JDBC_URL = "jdbc:mysql://172.31.22.43:3306/Mahi200562510";
+    private static final String USER = "Mahi200562510";
+    private static final String PASSWORD = "tK7pwBPi0L";
+
+    /**
+     * @person mahi
+     * method to read data for database.
+     * */
+    public static ObservableList<LaunchData> fetchData() {
+        ObservableList<LaunchData> data = FXCollections.observableArrayList();
+
+        try (
+                Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+                PreparedStatement ps = connection.prepareStatement("SELECT * FROM orbital_launches");
+                ResultSet resultSet = ps.executeQuery()
+        ) {
+            while (resultSet.next()) {
+                int year = resultSet.getInt("year");
+                String country = resultSet.getString("country");
+                int launchCount = resultSet.getInt("launch_count");
+                data.add(new LaunchData(year, country, launchCount));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    /**
+     * @person mahi
+     * method to grab data from database for selected year.
+     * */
+    public static ObservableList<LaunchData> fetchDataForYear(int year) {
+        ObservableList<LaunchData> data = FXCollections.observableArrayList();
+
+        try (
+                Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+                PreparedStatement ps = connection.prepareStatement("SELECT * FROM orbital_launches WHERE year = ?")
+        ) {
+            ps.setInt(1, year);
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                String country = resultSet.getString("country");
+                int launchCount = resultSet.getInt("launch_count");
+                data.add(new LaunchData(year, country, launchCount));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+}
