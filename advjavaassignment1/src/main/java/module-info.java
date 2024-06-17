83ebf2e0b7cb6f7a876b2bd2aedc46a046fcfb7b
Mahipatel7 <145039790+Mahipatel7@users.noreply.github.com>
2024-06-17 00:35:47 -0400

    Delete advjavaassignment1 directory

--- advjavaassignment1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.advjavaassignment1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.example.advjavaassignment1 to javafx.fxml;
-    exports com.example.advjavaassignment1;
-}
